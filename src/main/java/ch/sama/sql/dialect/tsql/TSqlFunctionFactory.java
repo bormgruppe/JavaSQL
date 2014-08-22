c1cd65aad2d5ad084a99292a950243d8330f74fe
Cabadath <Cabadath@localhost>
2014-08-22 23:02:22 +0200

    * Case When function added

--- src/main/java/ch/sama/sql/dialect/tsql/TSqlFunctionFactory.java
+++ src/main/java/ch/sama/sql/dialect/tsql/TSqlFunctionFactory.java
@@ -1,6 +1,7 @@
 package ch.sama.sql.dialect.tsql;
 
 import ch.sama.sql.dbo.Function;
+import ch.sama.sql.query.exception.BadParametersException;
 import ch.sama.sql.query.helper.Value;
 
 /*
@@ -14,6 +15,52 @@ public class TSqlFunctionFactory {
     
     public Function coalesce(Value lhs, Value rhs) {
         return new Function("COALESCE(" + lhs.toString() + ", " + rhs.toString() + ")");
+    }
+
+    public static class WhenThen {
+        private Value expression;
+        private Value value;
+
+        public WhenThen(Value expression, Value value) {
+            this.expression = expression;
+            this.value = value;
+        }
+
+        public boolean isElse() {
+            return expression == null;
+        }
+
+        public String toString() {
+            if (isElse()) {
+                return "ELSE " + value.toString();
+            } else {
+                return "WHEN " + expression.toString() + " THEN " + value.toString();
+            }
+        }
+    }
+    public WhenThen whenThen(Value expression, Value value) {
+        return new WhenThen(expression, value);
+    }
+    public Function caseWhen(Value expression, WhenThen... wts) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(CASE ");
+        builder.append(expression.toString());
+
+        for (int i = 0; i < wts.length; ++i) {
+            WhenThen wt = wts[i];
+
+            if (wt.isElse() && i != wts.length - 1) { // this also captures multiple ELSEs
+                throw new BadParametersException("ELSE must be the last argument");
+            }
+
+            builder.append("\n");
+            builder.append(wt.toString());
+        }
+
+        builder.append("\nEND)");
+
+        return new Function(builder.toString());
     }
 
     // extend at leisure
